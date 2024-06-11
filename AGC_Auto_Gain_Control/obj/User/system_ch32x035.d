9f8f6c4f1141c01055ed6dee9458613012ad0490
ZeEE <874074406@qq.com>
2024-06-11 22:13:54 +0800

    backup

---
+++ AGC_Auto_Gain_Control/obj/User/system_ch32x035.d
@@ -0,0 +1,70 @@
+User/system_ch32x035.o: ../User/system_ch32x035.c \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Core/core_riscv.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\User/system_ch32x035.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\User/ch32x035_conf.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_adc.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_awu.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_dbgmcu.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_dma.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_exti.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_flash.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_gpio.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_i2c.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_iwdg.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_pwr.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_rcc.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_spi.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_tim.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_usart.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_wwdg.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\User/ch32x035_it.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Debug/debug.h \
+ C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_misc.h
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Core/core_riscv.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\User/system_ch32x035.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\User/ch32x035_conf.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_adc.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_awu.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_dbgmcu.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_dma.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_exti.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_flash.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_gpio.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_i2c.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_iwdg.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_pwr.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_rcc.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_spi.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_tim.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_usart.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_wwdg.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\User/ch32x035_it.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Debug/debug.h:
+
+C:\MRS_DATA\workspace\CH32X035F7P6\Peripheral\inc/ch32x035_misc.h:
